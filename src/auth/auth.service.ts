03fb567a4f13f9e2000d4d42bef2397e526e0edf
Anisimov Stanislav <stanislav.anisimov.dev@gmail.com>
2024-02-10 17:00:20 +0400

    added base +bcrypt auth

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,28 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UsersRepository } from './users.repository';
+import { AuthCredentialDto } from './dto/create-user.dto';
+import * as bcrypt from 'bcrypt';
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectRepository(UsersRepository)
+    private usersRepository: UsersRepository,
+  ) {}
+
+  async signUp(authCredentialDto: AuthCredentialDto): Promise<void> {
+    return this.usersRepository.createUser(authCredentialDto);
+  }
+
+  async signIn(authCredentialDto: AuthCredentialDto): Promise<string> {
+    const { username, password } = authCredentialDto;
+
+    const user = await this.usersRepository.findOne({ where: { username } });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      return 'success';
+    } else {
+      throw new UnauthorizedException('Please check your login credentials');
+    }
+  }
+}
