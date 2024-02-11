12411fc4810f5eac310a80f84d32f3fc861e4c3d
Anisimov Stanislav <stanislav.anisimov.dev@gmail.com>
2024-02-11 10:24:34 +0400

    some changes for deploy

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { TransformInterceptor } from './transform.interceptor';
 import { Logger } from '@nestjs/common';
+import * as process from 'process';
 
 async function bootstrap() {
   const logger = new Logger();
@@ -11,8 +12,7 @@ async function bootstrap() {
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
-  const port = 3000;
-  await app.listen(port);
-  logger.log(`Application listening on port ${port}`);
+  await app.listen(process.env.PORT);
+  logger.log(`Application listening on port ${process.env.PORT}`);
 }
 bootstrap();
